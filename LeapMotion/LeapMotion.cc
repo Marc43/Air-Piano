4261553ed15af7afa9f211b01e9d51ee78540e7b
Daniel Martinez Bordes <dmartinezbordes@gmail.com>
2017-03-04 04:32:33 +0100

    Clase Leapmotion

---
+++ LeapMotion/LeapMotion.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+º
+#include <vector>
+#include <map>
+#include
+using namespace Leap;
+
+class LeapMotion {
+    private:
+        Controller dispositivo;
+        vector<Finger>
+
+    public:
+        virtual void onConnect(const Controller&);
+        virtual void onDisconnect(const Controller&);
+        virtual void onFrame(const Controller&);
+};
+
+void LeapEventListener::onConnect(const Controller& controller) {
+    std::cout << "Connected" << std::endl;
+    // Enable gestures, set Config values:
+    controller.enableGesture(Gesture::TYPE_SWIPE);
+    controller.config().setFloat("Gesture.Swipe.MinLength", 200.0);
+    controller.config().save();
+}
+
+//Not dispatched when running in a debugger
+void LeapEventListener::onDisconnect(const Controller& controller) {
+    std::cout << "Disconnected" << std::endl;
+}
+
+void LeapEventListener::onFrame(const Controller& controller) {
+    std::cout << "New frame available" << std::endl;
+    Frame frame = controller.frame();
+    // Process this frame's data...
+}
