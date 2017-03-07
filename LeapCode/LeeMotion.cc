c389363526d1ec7b43290ff64d626b7a78bcf847
Marc Domínguez de la Rocha <marcdominguezr@gmail.com>
2017-03-07 10:54:17 +0100

    Update LeeMotion.cc

--- LeapCode/LeeMotion.cc
+++ LeapCode/LeeMotion.cc
@@ -8,27 +8,27 @@ LeeMotion::LeeMotion () {
 }
 
 void LeeMotion::updateFrame () {
-    act = lpm.frame();
+    act = lpm.frame(); //We get the actual LPM Frame
 }
 
 bool LeeMotion::isConnected () {
-    return lpm.isConnected();
+    return lpm.isConnected(); //If the asynchronous connection is done, true, otherwise, false
 }
 
 FingerList LeeMotion::getFingers (const Hand &hand) {
-    return hand.fingers();
+    return hand.fingers(); //We get the FingerList
 }
 
 Vector LeeMotion::getFingerDirection (const Finger &finger) {
-    Bone b = finger.bone(Leap::Bone::TYPE_DISTAL);
+    Bone b = finger.bone(Leap::Bone::TYPE_DISTAL); //Returns the finger direction associated to the finger bone
     return b.direction();
 }
 
 HandList LeeMotion::getHands () {
-    HandList hl = act.hands();
+    HandList hl = act.hands(); //We get the HandList
     return hl;
 }
 
 Vector LeeMotion::getHandNormal (const Hand &hand) {
-    return hand.palmNormal();
+    return hand.palmNormal(); //The Normal of the hand
 }
